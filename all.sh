cc194068159d7292035b845a6a92c018525b2e8d
hanukkah1 <127838694+hanukkah1@users.noreply.github.com>
2023-05-24 08:40:26 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -266,7 +266,7 @@ function run {
     #Prepare for running
     sed_for run $BASEPATH
 
-    cd $BASEPATH && python launch.py --listen --share --xformers --opt-sdp-attention --disable-safe-unpickle --theme dark --enable-insecure-extension-access --no-hashing --no-half-vae --clip-models-path $BASEPATH/models/CLIP
+    cd $BASEPATH && python launch.py --xformers --opt-sdp-attention --disable-safe-unpickle --theme dark --enable-insecure-extension-access --no-hashing --no-half-vae --no-half --clip-models-path $BASEPATH/models/CLIP
 }
 
 BASEPATH=/content/drive/MyDrive/SD
